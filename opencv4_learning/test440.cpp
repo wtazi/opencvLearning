2d4a6ef718a6a0fd74cfd90adf6e0e1beb19b8ea
wtazi <support@bljx.org>
2021-06-29 21:13:15 +0800

    图像读取与显示-显示时可调整图片大小

--- opencv4_learning/test440.cpp
+++ opencv4_learning/test440.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int main() {
 	Mat src = imread("D:/images/1-1.jpg");//读取图像	
-	imshow("input", src);//(窗口名称，窗口要显示的图像对象)
+	namedWindow("输入窗口", WINDOW_FREERATIO);//创建一个窗口.FREERATIO可以调整图像大小,是显示图像的更好选择
+	imshow("输入窗口", src);//(窗口名称，窗口要显示的图像对象)
 	waitKey(0);//显示需要，0代表阻塞，1表示停顿1ms
 	destroyAllWindows();//对前面所有创建的显示窗口进行销毁
 	return 0;
