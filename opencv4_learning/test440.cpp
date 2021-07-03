bfbf0a0041585fd5fcb4086f1de1821d8c641ca9
wtazi <support@bljx.org>
2021-07-03 18:15:37 +0800

    显示灰度图像

--- opencv4_learning/test440.cpp
+++ opencv4_learning/test440.cpp
@@ -5,7 +5,7 @@ using namespace cv;//引用命名空间
 using namespace std;
 
 int main() {
-	Mat src = imread("D:/images/1-1.jpg");//读取图像	
+	Mat src = imread("D:/images/1-1.jpg",IMREAD_GRAYSCALE);//读取图像	
 	namedWindow("输入窗口", WINDOW_FREERATIO);//创建一个窗口.FREERATIO可以调整图像大小,是显示图像的更好选择
 	imshow("输入窗口", src);//(窗口名称，窗口要显示的图像对象)
 	waitKey(0);//显示需要，0代表阻塞，1表示停顿1ms
